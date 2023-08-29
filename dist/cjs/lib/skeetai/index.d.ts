e3fdad3fd8afbbf795f30f3b35578efec5c0e552
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-08-29 09:43:14 +0200

    update: release v1.4.3

--- dist/cjs/lib/skeetai/index.d.ts
+++ dist/cjs/lib/skeetai/index.d.ts
@@ -3,7 +3,7 @@ import { VertexAI } from '../vertexai';
 /**
  * Configuration options for initializing a SkeetAI instance.
  */
-interface SkeetAIOptions {
+export interface SkeetAIOptions {
     /**
      * AI platform type (either 'VertexAI' or 'OpenAI'). Default is 'VertexAI'.
      */
--- dist/esm/lib/version.js
+++ dist/esm/lib/version.js
@@ -1,2 +1,2 @@
-export const VERSION = '1.4.2';
+export const VERSION = '1.4.3';
 //# sourceMappingURL=version.js.map
